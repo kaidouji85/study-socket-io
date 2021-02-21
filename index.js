bc9a057a970daa00c7298967852a037be8791ed9
竹内佑介 <kaidouji85@gmail.com>
2021-02-21 21:07:22 +0900

    ブラウザからnode.jsサーバにメッセージ送信できるようになった

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, () => {
